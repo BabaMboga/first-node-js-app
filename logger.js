cd384cc1f56cc759364eec4d46f9d7fd5cacdbe7
Ayim William <odhisayim@gmail.com>
2023-10-24 12:41:32 +0300

    learned about wrapper functions

--- logger.js
+++ logger.js
@@ -1,3 +1,6 @@
+console.log(__filename);
+console.log(__dirname);
+
 var url = "http:mylogger.io/log";
 
 function log(message) {
@@ -8,3 +11,10 @@ function log(message) {
 module.exports = log;
 // module.exports.endPoint = url;
 // commented out because we want to keep the url private but as u can see one can export it using a different name than one initiated
+
+// module.exports.log = log;
+// exports.log = log;
+// the above two would still work 
+
+// exports = log;
+// this wouldnt work as exports references module and we cannot change that
