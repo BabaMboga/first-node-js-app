56e130826166d0f59f9cc5e170c0ba4f07406f73
Ayim William <odhisayim@gmail.com>
2023-10-24 18:01:48 +0300

    finished on os module

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ sayHello('Little girl');
 // It won't work because in node.js the window and document object don't exist like it does for js in browsers
 
 logger('message');
+
+// the path module begins here
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+// the os module begins here
+
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log('Total Memory: ' + totalMemory);
+
+// the above can be simplified using template strings below
+
+console.log(`Total Memory: ${totalMemory}`)
+console.log(`Free Memory: ${freeMemory}`)
